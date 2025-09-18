e50f06ca180f7b3f152f030910f0b805259df65e
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2025-09-18 09:21:42 +0100

    add implementation to post, get and delete skills

--- app.py
+++ app.py
@@ -66,15 +66,57 @@ def education():
     return jsonify({})
 
 
-@app.route('/resume/skill', methods=['GET', 'POST'])
-def skill():
+@app.route("/resume/skill", methods=["GET", "POST"])
+@app.route('/resume/skill/<int:skill_id>', methods=['DELETE'])
+def skill(skill_id = None):
     '''
     Handles Skill requests
     '''
     if request.method == 'GET':
-        return jsonify({})
+        return jsonify([skill.__dict__ for skill in data["skill"]]), 200
 
     if request.method == 'POST':
-        return jsonify({})
+        json_data = request.json
+        try:
+            name = json_data["name"]
+            proficiency = json_data["proficiency"]
+            logo = json_data["logo"]
+
+            new_skill = Skill(name, proficiency, logo)
+
+            data["skill"].append(new_skill)
+
+            return jsonify(
+                {"id": len(data["skill"]) - 1}
+            ), 201
+
+        except KeyError:
+            return jsonify({"error": "Invalid request"}), 400
+
+        except TypeError as e:
+            return jsonify({"error": str(e)}), 400
+    if request.method == "DELETE":
+        try:
+            if skill_id is None or skill_id < 0 or skill_id >= len(data["skill"]):
+                return jsonify({"message": "Resource doesn't exist"}), 404
+            else:   
+                del data['skill'][skill_id]
+                return jsonify({"message": "Skill Successfully Deleted"}), 200
+
+        except Exception as e:      
+            return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
     return jsonify({})
+      
+@app.route('/resume/skill/<int:skill_id>', methods=['GET'])
+def get_skill(skill_id):
+    '''
+    Get a specific skill
+    '''
+
+    if request.method == "GET":
+        try:
+            return jsonify(data["skill"][skill_id].__dict__)
+        except IndexError:
+            return jsonify({"error": "Skill not found"}), 404
+
